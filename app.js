e6a90f5428c4b477fbbf787ab0d9a30620fed40f
Ram_personal <salunkherc@gmail.com>
2025-01-08 09:06:30 +0530

    episode 02

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
 const heading = React.createElement("h1", {id:"heading", xyz:"xyz"}, "Hello World from React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // console.log(heading); // return object
@@ -10,16 +13,17 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
  * </div> 
  */
 
-const parent = React.createElement(
-    "div", 
-    {id: "parent"},
-    React.createElement(
-        "div", {id: "child"},
-        [
-            React.createElement("h1", {}, "I'am h1 tag"), 
-            React.createElement("h2", {}, "I'am h2 tag")
-        ]       
-    ) 
- )
+const parent = React.createElement("div", {id:"parent"}, [
+    React.createElement("div", 
+    {id: "child"}, [
+      React.createElement("h1", {}, "This is react"), 
+      React.createElement("h2", {}, "I'am h2 tag")
+    ]),
+    React.createElement("div", 
+    {id: "child"}, [
+      React.createElement("h1", {}, "I'am h1 tag"), 
+      React.createElement("h2", {}, "I'am h2 tag")
+    ])
+])
 
 root.render(parent);
